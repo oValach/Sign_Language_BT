bfe40786a4381e8726a415d41d484e0aa439561a
oValach <valach.ondra@seznam.cz>
2021-05-08 15:44:05 +0200

    hamming tests update

--- start_hamming.py
+++ start_hamming.py
@@ -5,9 +5,9 @@ paths = 'data/paths_.txt'
 bvh_dir,bvh_dict,source_dir,path_jointlist,path_chosen_joints,path_dictionary,path_metadata,path_trajectory,path_output,joint_list,meta,traj = load(paths)
 
 alg_type = 'method_combination'
-resample_method = 'fourier'
+resample_method = 'interpolation'
 int_method = 'linear'
-distance_method = 'euclidean'
+distance_method = 'hamming'
 order = 'toShorter'
 
 distance_matrix = compute(path_output, path_trajectory, path_chosen_joints, alg_type=alg_type, order=order, 

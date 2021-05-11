32bf7dac7c8d68570e4b6e1e04e8ccad21cfb480
oValach <valach.ondra@seznam.cz>
2021-05-11 22:59:22 +0200

    tests

--- start_minkowsky.py
+++ start_minkowsky.py
@@ -5,9 +5,9 @@ paths = 'data/paths_.txt'
 bvh_dir,bvh_dict,source_dir,path_jointlist,path_chosen_joints,path_dictionary,path_metadata,path_trajectory,path_output,joint_list,meta,traj = load(paths)
 
 alg_type = 'method_combination'
-resample_method = 'fourier'
+resample_method = 'interpolation'
 int_method = 'linear'
-distance_method = 'minkowsky4'
+distance_method = 'minkowsky7'
 order = 'toShorter'
 
 distance_matrix = compute(path_output, path_trajectory, path_chosen_joints, alg_type=alg_type, order=order, 
